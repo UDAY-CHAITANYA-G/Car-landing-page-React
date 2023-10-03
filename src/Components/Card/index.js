850aa66013c99a3e7fc9bb43e7cbf392f7465da0
UDAY-CHAITANYA-G <udaychaitanyagurvindapalli@gmail.com>
2023-10-04 01:16:37 +0530

    static images to car is added

--- src/Components/Card/index.js
+++ src/Components/Card/index.js
@@ -1,12 +1,29 @@
-import React from "react";
+import React,{useState,useEffect} from "react";
 import {FiHeart} from "react-icons/fi"
 
 import "./styles.css";
 
+const IMAGES=["https://img.freepik.com/premium-photo/bangkok-thailand-08082022-lamborghini-luxury-super-car-fast-sports-premium-lighting-background-3d-illustration_67092-1599.jpg","https://imageio.forbes.com/specials-images/imageserve/5d35eacaf1176b0008974b54/2020-Chevrolet-Corvette-Stingray/0x0.jpg?format=jpg&crop=4560,2565,x790,y784,safe&width=960","https://cdn.motor1.com/images/mgl/g440ng/s3/rimac-nevera.jpg"]
+
 export default function Card({car,...props}){
+
+    const [index,setIndex] = useState(0);
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+          setIndex((index) => (index + 1) % IMAGES.length);
+        }, 2000);
+    
+        return () => {
+          clearInterval(intervalId);    
+        };
+      }, []);
+
     return (
     <div className="Car-box">
-        <div className="Car-image-box"></div>
+        <div className="Car-image-box">
+            <img src={IMAGES[index]}></img>
+        </div>
         <div className="Car-details">
             <div className="Car-name">
                 <h3>{car.Name}</h3>
